001881b05066cdaec013e2fd142bfad13746d6fc
Valera Abramchuk <64899085+valera-abramchuk@users.noreply.github.com>
2021-01-14 16:13:41 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+ 
+ 
+
+ let stpbl1 = document.getElementById("stepbl1");
+ let stpbrdr1 = document.getElementById("stepbrdr1");
+
+ let stpbl2 = document.getElementById("stepbl2");
+ let stpbrdr2 = document.getElementById("stepbrdr2");
+
+ let stpbl3 = document.getElementById("stepbl3");
+ let stpbrdr3 = document.getElementById("stepbrdr3");
+
+
+ stpbl1.onmouseover = function() {
+ 	stepbrdr1.style.borderBottom = "3px solid white";
+
+ }
+  stpbl1.onmouseout = function() {
+ 	stepbrdr1.style.borderBottom = "3px solid black";
+
+ };
+
+ stpbl2.onmouseover = function() {
+ 	stepbrdr2.style.borderBottom = "3px solid white";
+
+ }
+  stpbl2.onmouseout = function() {
+ 	stepbrdr2.style.borderBottom = "3px solid black";
+
+ };
+ stpbl3.onmouseover = function() {
+ 	stepbrdr3.style.borderBottom = "3px solid white";
+
+ }
+  stpbl3.onmouseout = function() {
+ 	stepbrdr3.style.borderBottom = "3px solid black";
+
+ };
